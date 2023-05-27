eaa3d1609f441029b1cb4c6dc78daaabb1223a42
gokul <gokulchandan18@gmail.com>
2023-05-27 11:07:29 +0530

    a

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("🔌 MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log("🔌 MongoDB connected");
+  } catch (error) {
+    console.log("⛔ ~ file: database.js:23 ~ connectToDB ~ error:", error);
+  }
+};
